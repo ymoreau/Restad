644f33f72056e3b4e4808c20eaeb8a518481308b
Yoann Moreau <moreau.yo@gmail.com>
2012-01-25 11:13:33 +0100

    Prototype 2

--- sql/database.sql
+++ sql/database.sql
@@ -1,62 +1,87 @@
+
+CREATE LANGUAGE plpgsql;
+
 --------------------------------------------------------------------------------
 -- Table : docs
 drop table if exists docs cascade;
 create table docs
 (
-    id_doc int,
+    id_doc serial,
     doc_name varchar(256),
     text text,
-    text_tsvector tsvector
+    text_tsvector tsvector,
+    primary key(id_doc)
 );
+
+CREATE OR REPLACE FUNCTION update_tsvector() RETURNS trigger AS $$
+begin
+  new.text_tsvector := to_tsvector(new.text);
+  return new;
+end
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
+ON docs FOR EACH ROW EXECUTE PROCEDURE update_tsvector();
+
+CREATE INDEX doc_tsvector_idx ON docs USING gin (text_tsvector);
 
 --------------------------------------------------------------------------------
 -- Table : tag_names
 drop table if exists tag_names cascade;
 create table tag_names
 (
-    id_tag_name int,
-    tag_name varchar(256) --, unique(tag_name)
+    id_tag_name serial,
+    tag_name varchar(256) unique, 
+    primary key(id_tag_name)
 );
+
+--create index tag_names_index on tag_names(tag_name);
 
 --------------------------------------------------------------------------------
 -- Table : tags
 drop table if exists tags cascade;
 create table tags
 (
-    id_tag bigint,
-    id_doc int, -- references docs(id_doc)
-    id_tag_name int, -- references tag_names(id_tag_name)
-    tag_num int not null,
-    parent_tag int, -- References tags(id_tag)
+    id_doc int references docs(id_doc),
+    id_tag int,
+    id_tag_name int references tag_names(id_tag_name),
+    tag_order_position int not null,
+    parent_id int,
     starting_offset int,
     ending_offset int
+    -- primary key(id_tag,id_doc)
 );
+
+create index tags_id_doc_index on tags(id_doc);
+create index tags_id_tag_index on tags(id_tag);
+create index tags_id_tag_name_index on tags(id_tag_name);
+create index tags_parent_index on tags(parent_id);
 
 --------------------------------------------------------------------------------
 -- Table : attribute_names
 drop table if exists attribute_names cascade;
 create table attribute_names
 (
-    id_attribute_name int,
-    attribute_name varchar(256) -- unique(attribute_name)
+    id_attribute_name serial,
+    attribute_name varchar(256) unique,
+    primary key(id_attribute_name)
 );
 
---------------------------------------------------------------------------------
--- Table : attribute_values
-drop table if exists attribute_values cascade;
-create table attribute_values
-(
-    id_attribute_value int,
-    attribute_value text -- unique(attribute_value)
-);
+--create index attribute_names_index on attribute_names(attribute_name);
 
 --------------------------------------------------------------------------------
 -- Table : tag_attributes
 drop table if exists tag_attributes cascade;
 create table tag_attributes
 (
-    id_tag bigint, -- references tags(id_tag)
-    id_attribute_name int, -- references attribute_names(id_attribute_name)
-    id_attribute_value int -- references attribute_values(id_attribute_value)
+    id_doc int,
+    id_tag int, 
+    id_attribute_name int references attribute_names(id_attribute_name),
+    attribute_value text
+    -- primary key(id_tag,id_doc)
 );
+
+create index attributes_id_doc_index on tag_attributes(id_doc);
+create index attributes_id_tag_index on tag_attributes(id_tag);
+create index attributes_id_attname_index on tag_attributes(id_attribute_name);
 
---
+++ sql/restad.sql
@@ -0,0 +1,99 @@
+--------------------------------------------------------------------------------
+-- Returns the character offset beginning of the term_pos'th term in text
+CREATE OR REPLACE FUNCTION get_offset(doctext text, term_pos int) RETURNS int AS $$
+DECLARE
+    tsdebug_row record;
+    raw_text text := '';
+    lexeme_count int := 0;
+    term_offset int := 0;
+BEGIN
+    FOR tsdebug_row IN SELECT token,lexemes FROM ts_debug(doctext) LOOP
+        IF tsdebug_row.lexemes IS NOT NULL THEN
+            lexeme_count := lexeme_count + 1;
+        END IF;
+        IF lexeme_count = term_pos THEN
+            RETURN char_length(raw_text);
+        END IF;
+
+        raw_text := raw_text || tsdebug_row.token;
+    END LOOP;
+    
+    RETURN -1;
+END
+$$ LANGUAGE 'plpgsql';
+
+--------------------------------------------------------------------------------
+-- Returns a XPath-like string for the given tag_id
+CREATE OR REPLACE FUNCTION get_path(id INTEGER) RETURNS TEXT AS $$
+  SELECT '/' || path FROM
+    (WITH RECURSIVE pathto(path, id) AS (
+      SELECT CAST(tag_name || '[' || tag_num::text || ']' AS TEXT), parent_tag FROM tags NATURAL JOIN tag_names WHERE id_tag = $1
+      UNION
+      SELECT CAST(names.tag_name || '[' || tags.tag_num::text || ']/' || pathto.path AS TEXT), parent_tag
+        FROM tags NATURAL JOIN tag_names AS names, pathto WHERE tags.id_tag = pathto.id)
+      SELECT * FROM pathto) AS pathto(path, id)
+    WHERE id IS NULL;
+$$ LANGUAGE 'sql';
+
+--------------------------------------------------------------------------------
+-- Returns the term positions of the given word for the given tsvector
+CREATE OR REPLACE FUNCTION get_term_positions(word VARCHAR, tsvect tsvector) RETURNS TABLE(r_pos int) AS $$
+DECLARE
+    term text;
+    str_positions text;
+    pos_int integer;
+BEGIN
+    SELECT token INTO STRICT term FROM substring(to_tsvector(word)::text from '''(.+)''') as token;
+    SELECT strpos INTO str_positions FROM substring(tsvect::text from '''' || term || ''':([0-9,]+)') as strpos;
+    FOR pos_int IN SELECT pos_str::int FROM regexp_split_to_table(str_positions, ',') as pos_str LOOP
+        r_pos := pos_int;
+        RETURN NEXT;
+    END LOOP;
+    RETURN;
+END
+$$ LANGUAGE 'plpgsql';
+
+--------------------------------------------------------------------------------
+-- Returns the result_limit highest ranked documents for the given tsquery
+CREATE OR REPLACE FUNCTION xdocs_rank(query tsquery, result_limit int default 1000) RETURNS TABLE(idoc int, tsrank float) AS $$
+DECLARE
+   docrow record;
+BEGIN
+    FOR docrow IN SELECT id_doc, ts_rank(text_tsvector, query) AS rank, text_tsvector, text FROM docs
+            WHERE text_tsvector @@ query ORDER BY rank DESC LIMIT result_limit LOOP
+
+        idoc := docrow.id_doc;
+        tsrank := docrow.rank;
+        RETURN NEXT;
+
+    END LOOP;
+    RETURN;
+END
+$$ LANGUAGE 'plpgsql';
+
+--------------------------------------------------------------------------------
+-- Returns the tag id of the LCA for the given term in the given document
+CREATE OR REPLACE FUNCTION xterm_match(qterm text, doc_id int) RETURNS TABLE(nearest_tag_id int) AS $$
+DECLARE
+    docrow record;
+    pos int;
+    offset int;
+BEGIN
+    SELECT text, text_tsvector INTO STRICT docrow FROM docs WHERE id_doc = doc_id;
+    FOR pos IN SELECT get_term_positions(qterm, docrow.text_tsvector) FROM docs WHERE id_doc = doc_id LOOP
+        offset := get_offset(docrow.text, pos); -- Map to the offset position
+        -- Get the nearest tag id for this term position in the current doc
+        SELECT t.id_tag INTO nearest_tag_id
+          FROM (SELECT id_tag, parent_tag FROM tags WHERE id_doc = doc_id AND
+                starting_offset <= offset AND ending_offset > offset) AS t
+          WHERE t.id_tag NOT IN 
+              (SELECT parent_tag FROM tags WHERE id_doc = doc_id AND
+               parent_tag IS NOT NULL AND starting_offset <= offset AND ending_offset > offset)
+          ;
+        RETURN NEXT;
+    END LOOP;
+    
+    RETURN;
+END
+$$ LANGUAGE 'plpgsql';
+
